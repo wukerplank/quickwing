33811c8768333601f9d7266d3a2da18285860b64
Thomas Esterer <esterer.thomas@gmail.com>
2013-04-17 09:54:14 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quickwing::Application.config.secret_token = '30876ee4b29229df5be7bb16968c99fc25c6f0130ef844f973586a00298e676c1642849d495c6b6dfba1d47b8a2b6e1bc0b17b6dbafaa02911b0442e47c2462d'
