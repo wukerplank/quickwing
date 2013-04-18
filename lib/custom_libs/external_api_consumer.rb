d360d8a281dc4ce3d3b56b0a7e3e0d05bc3123ac
Christoph Edthofer <wukerplank@edthofer.at>
2013-04-18 09:15:23 +0200

    minor change to the API Consumer

--- lib/custom_libs/external_api_consumer.rb
+++ lib/custom_libs/external_api_consumer.rb
@@ -1,27 +1,25 @@
 class ExternalApiConsumer
-	def initialize (queue_name)
-		@queue_name = queue_name
+  def initialize (queue_name, &block)
+    @queue_name = queue_name
 
-		@client = Bunny.new("amqp://#{ENV['RABBIT_MQ_USER']}:#{ENV['RABBIT_MQ_PASSWORD']}@#{ENV['RABBIT_MQ_HOST']}:#{ENV['RABBIT_MQ_PORT']}")
-		@client.start
+    @client = Bunny.new("amqp://#{ENV['RABBIT_MQ_USER']}:#{ENV['RABBIT_MQ_PASSWORD']}@#{ENV['RABBIT_MQ_HOST']}:#{ENV['RABBIT_MQ_PORT']}")
+    @client.start
 
-		@channel  = @client.create_channel
-		@exchange = @channel.fanout(ENV['SEARCH_JOB_EXCHANGE_NAME'])
-	end
+    @channel  = @client.create_channel
+    @exchange = @channel.fanout(ENV['SEARCH_JOB_EXCHANGE_NAME'])
+    
+    queue = @channel.queue(@queue_name).bind(@exchange).subscribe do |delivery_info, metadata, payload|
+      json = JSON.parse(payload)
+      
+      puts "received: #{json.inspect}"
+      
+      yield json
+    end
 
-	def bunny_loop
-		queue = @channel.queue(@queue_name).bind(@exchange).subscribe do |delivery_info, metadata, payload|
-		    json = JSON.parse(payload)
-		    
-		    puts "received: #{json.inspect}"
-
-		    yield json
-		end
-
-		loop do
-    		# wait until this process gets killed
-		end
+    loop do
+      # wait until this process gets killed
+    end
   
-		@client.close
-	end
+    @client.close
+  end
 end
